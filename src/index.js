c3d563403af3566ebe2c496ede27843196185ea0
factcondenser <markpan@live.com>
2018-06-01 23:48:23 -0500

    Add placeholder for row and col info

--- src/index.js
+++ src/index.js
@@ -173,7 +173,7 @@ class Game extends React.Component {
     const moves = history.map((step, move) => {
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{this.renderMoveDesc(move)}</button>
+          <button onClick={() => this.jumpTo(move)}>{this.renderMoveDesc(move)}'(row, col)'</button>
         </li>
       );
     });
